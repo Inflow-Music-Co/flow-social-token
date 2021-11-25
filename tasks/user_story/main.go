8b4361b620cb8bfbf7f050f2a4a7b4a274f91db9
Richard Melkonian <r.v.melkonian@gmail.com>
2021-11-25 15:34:32 +0000

    updated user_story and added comments

--- tasks/user_story/main.go
+++ tasks/user_story/main.go
@@ -17,6 +17,8 @@ func main() {
 	g := gwtf.NewGoWithTheFlowInMemoryEmulator()
 
 	//-------------------------------------------------//
+	// ------ SETUP AND MINT FUSD ---------------------//
+	//-------------------------------------------------//
 
 	//Setup FUSD Vaults for both accounts
 	g.TransactionFromFile("setup_fusd_vault").SignProposeAndPayAs("first").RunPrintEventsFull();
@@ -28,49 +30,54 @@ func main() {
 	//Admin Account deposits minter into first account
 	g.TransactionFromFile("deposit_fusd_minter").SignProposeAndPayAs("account").AccountArgument("first").RunPrintEventsFull();
 
-	// First Account Mints and deposits in one transaction
-	g.TransactionFromFile("mint_fusd").SignProposeAndPayAs("first").UFix64Argument("100.00").AccountArgument("first").RunPrintEventsFull();
+	//first account Mints and deposits in one transaction
+	g.TransactionFromFile("mint_fusd").SignProposeAndPayAs("first").UFix64Argument("999999.00").AccountArgument("first").RunPrintEventsFull();
 
 	//Log balance
 	fusdFirstAccountBalance := g.ScriptFromFile("get_fusd_balance").AccountArgument("first").RunFailOnError()
 	log.Printf("FUSD balance of account 'first account' %s", fusdFirstAccountBalance)	
 
 	//-------------------------------------------------//
+	//--------- SETUP AND MINT SOCIAL TOKEN -----------//
+	//-------------------------------------------------//
 
 	//Setup SocialToken Vaults for both accounts
-
 	g.TransactionFromFile("setup_social_vault").SignProposeAndPayAs("first").RunPrintEventsFull()
 	g.TransactionFromFile("setup_social_vault").SignProposeAndPayAs("account").RunPrintEventsFull()
 
-	g.TransactionFromFile("setup_st_minter").SignProposeAndPayAs("first").RunPrintEventsFull()
-	g.TransactionFromFile("deposit_st_minter").SignProposeAndPayAs("account").AccountArgument("first").RunPrintEventsFull()
+	//First Account sets up Social Minter
+	g.TransactionFromFile("setup_social_minter").SignProposeAndPayAs("first").RunPrintEventsFull()
+
+	//Admin Account deposits  minter into first account
+	g.TransactionFromFile("deposit_social_minter").SignProposeAndPayAs("account").AccountArgument("first").RunPrintEventsFull()
 	
-	g.TransactionFromFile("mint_st").SignProposeAndPayAs("first").AccountArgument("first").UFix64Argument("33.0").RunPrintEventsFull()
+	//first account mints and deposits in one transaction
+	g.TransactionFromFile("mint_social").SignProposeAndPayAs("first").AccountArgument("first").UFix64Argument("33.0").RunPrintEventsFull()
 
-	// Run script that returns
-	resultAccount := g.ScriptFromFile("get_st_balance").AccountArgument("account").RunFailOnError()
-	log.Printf("Script returned %s", resultAccount)
+	// script returns social token balanace for account Account
+	AccountSocialBalance := g.ScriptFromFile("get_social_balance").AccountArgument("account").RunFailOnError()
+	log.Printf("Script returned %s", AccountSocialBalance)
 
-	// Run script that returns
-	resultFirst := g.ScriptFromFile("get_st_balance").AccountArgument("first").RunFailOnError()
-	log.Printf("Script returned %s", resultFirst)
+	// script returns social token balanace for account First
+	FirstSocialBalance := g.ScriptFromFile("get_social_balance").AccountArgument("first").RunFailOnError()
+	log.Printf("Script returned %s", FirstSocialBalance)
 
-	g.TransactionFromFile("setup_st_burner").SignProposeAndPayAs("first").RunPrintEventsFull()
-	g.TransactionFromFile("deposit_st_burner").SignProposeAndPayAs("account").AccountArgument("first").RunPrintEventsFull()
-	g.TransactionFromFile("burn_tokens").SignProposeAndPayAs("first").UFix64Argument("10.0").RunPrintEventsFull()
-	// g.TransactionFromFile("mint_st").SignProposeAndPayAs("account").AccountArgument("first").UFix64Argument("33.0").RunPrintEventsFull()
+	//-------------------------------------------------//
+	//--------- SETUP AND BURN SOCIAL TOKEN -----------//
+	//-------------------------------------------------//
 
-	// Run script that returns
-	resultAccountEnd := g.ScriptFromFile("get_st_balance").AccountArgument("account").RunFailOnError()
+	//First Account sets up Social Minter
+	g.TransactionFromFile("setup_social_burner").SignProposeAndPayAs("first").RunPrintEventsFull()
+
+	//Admin Account deposits  burner into first account
+	g.TransactionFromFile("deposit_social_burner").SignProposeAndPayAs("account").AccountArgument("first").RunPrintEventsFull()
+
+	//First account burns tokens
+	g.TransactionFromFile("burn_tokens").SignProposeAndPayAs("first").UFix64Argument("1.111111").RunPrintEventsFull()
+
+	//script returns social balance after burn for account first
+	resultAccountEnd := g.ScriptFromFile("get_social_balance").AccountArgument("first").RunFailOnError()
 	log.Printf("Script returned %s", resultAccountEnd)
-
-	// Run script that returns
-	resultFirstEnd := g.ScriptFromFile("get_st_balance").AccountArgument("first").RunFailOnError()
-	log.Printf("Script returned %s", resultFirstEnd)
-
-	// Run script that returns
-	fusdFirstBalance := g.ScriptFromFile("get_fusd_balance").AccountArgument("first").RunFailOnError()
-	log.Printf("FUSD balance of account 'first' %s", fusdFirstBalance)
 	
 
 }
